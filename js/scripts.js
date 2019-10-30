7de51726f2d00abeecc9a9447698a6dbc6671b0d
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-10-29 20:35:37 -0700

    finish backend

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,31 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
 
+
     var year = parseInt($("input#year").val());
-    var result = function(year) {
+    // unrefactored function aka the long way to do it.
+    // var leapYear = function(year) {
+    //   if (year % 100 === 0) {
+    //     return false;
+    // } else if (year % 4 === 0) {
+    //   return true;
+    // } else if (year % 400 === 0) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    // };
 
-
-
+    // refactored function.
+    var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      return true;
+    } else {
       return false;
-      };
+    }
+  };
+
+    var result = leapYear(year);
       $("#result").text(result);
-
-
   });
 });
